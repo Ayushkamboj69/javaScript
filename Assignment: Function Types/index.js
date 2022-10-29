8de1da0193b8d102cf007c34626ca150e60070a1
Ayushkamboj69 <114069208+Ayushkamboj69@users.noreply.github.com>
2022-10-29 23:13:57 +0530

    Add files via upload

---
+++ Assignment: Function Types/index.js
@@ -0,0 +1,272 @@
+// -Look at the example below and do the same for other problems.
+
+/*
+Write a function named convertToString that accepts a number and converts the number into a string (hint: use String()) and return the converted value.
+
+Do the following for the given problem:
+*/
+
+// - Write a Function Decleration
+// function convertToString (n){
+//     return String(n);
+// }
+
+// - Write a Function Expression
+// let convertToString = function(n){
+//     return String(n);
+// }
+
+// - Write an Arrow Function without curly brackets (if possible)
+// convertToString = function(n) => String(n)
+// - Write an Arrow Function with curly brackets
+
+// - Execute the function
+// convertToString(21);
+
+// - Execute the function and store the return value in a variable.
+// let returnValue = convertToString(23);
+
+// - What is the typeof returnValue
+// typeof returnValue is "string"
+
+/* 1. Add one to the given value
+
+Write a function named `addOne` that accepts a number, adds one to the given value and`return` it(hint: use return keyword).
+
+Do the following with above problem.
+*/
+
+// - Write a Function Declaration
+// function addOne (number){
+//   return number + 1;
+// }
+
+// - Write a Function Expression
+
+// let addOne = function(number){
+//   return number + 1;
+// }
+
+// - Write an Arrow Function without curly brackets(if possible)
+
+// let addOne = (number) => number+1;
+
+// - Write an Arrow Function with curly brackets
+
+// let addOne = (number) =>  {return number+1};
+
+// - Execute the function
+
+// addOne();
+
+// - Execute the function and store the return value in a variable.
+
+// let addOneFunction =addOne();
+
+// - What is the typeof returnValue
+
+// typeof(addOneFunction); //number
+
+/* 2. Subtract one
+
+Write a function named `substractOne` that accepts a number, substracts one from the given value and return it. (hint: use return keyword)
+
+Do the following with above problem.
+*/
+
+// - Write a Function Declaration
+// function substractOne(number){
+// return number -1;
+// }
+
+// - Write a Function Expression
+
+// let substractOne = function(number){
+// return number- 1 ;
+// }
+
+// - Write an Arrow Function without curly brackets(if possible)
+
+// let substactOne = (number) => number-1;
+
+// - Write an Arrow Function with curly brackets
+
+// let substactOne = (number) => {return number-1};
+
+// - Execute the function
+
+// substractOne();
+
+// - Execute the function and store the return value in a variable.
+
+// let substractOneFunction = substractOne();
+
+// - What is the typeof returnValue
+
+// typeof(substractOneFunction) // number
+
+/* 3. Add two numbers
+
+Write a function named `sum` that accepts two numbers and returns the sum of them.
+
+Do the following with above problem.
+*/
+
+// - Write a Function Declaration
+
+// function sum(numberOne, numberTwo) {
+//   return numberOne + numberTwo;
+// }
+
+// - Write a Function Expression
+
+// let sum = function (numberOne, numberTwo) {
+  // return numberOne + numberTwo;
+// };
+
+// - Write an Arrow Function without curly brackets(if possible)
+
+// let sum = (numberOne, numberTwo) => numberOne + numberTwo;
+
+// - Write an Arrow Function with curly brackets
+
+// let sum = (numberOne, numberTwo) => numberOne + numberTwo;
+
+// - Execute the function
+
+// sum();
+
+// - Execute the function and store the return value in a variable
+
+// let sumFunction = sum();
+
+// - What is the typeof returnValue
+
+// typeof (sumfunction);
+/* 4. Square of the given value
+
+Write a function named `square` that accepts a number and returns the square of the number.
+
+Do the following with above problem.
+*/
+
+// - Write a Function Declaration
+
+// function square(number){
+  // return number * number;
+// }
+
+// - Write a Function Expression
+
+// const square = function(number){
+  // return number * number;
+// }
+
+// - Write an Arrow Function without curly brackets(if possible)
+
+// const square = (number) => number * number;
+
+// - Write an Arrow Function with curly brackets
+
+// const square = (number) => {return number * number};
+
+// - Execute the function
+
+// square();
+
+// - Execute the function and store the return value in a variable
+
+// let squareFunction = square();
+
+// - What is the typeof returnValue
+
+// typeof(squareFunction);
+
+/* 5. Find isGreater
+
+Write a function named isGreater that accepts two numbers`x` and`y` and returns`true` if x is greater than y or return `false`.
+
+Do the following with above problem.
+*/
+
+// - Write a Function Declaration
+
+// function isGreater (x,y){
+  // return x>y?x:y;
+// }
+
+// - Write a Function Expression
+
+// const isGreater = function (x,y){
+  // return x>y?x:y;
+// }
+
+// - Write an Arrow Function without curly brackets(if possible)
+
+// const isGreater = (x,y) => x>y?x:y;
+
+// - Write an Arrow Function with curly brackets
+
+// const isGreater = (x,y) => x>y?x:y;
+
+// - Execute the function
+
+// isGreater();
+
+// - Execute the function and store the return value in a variable
+
+// let isGreaterFunction = isGreater();
+
+// - What is the typeof returnValue
+
+// typeof(isGreaterFunction); // number
+
+/* 6.
+
+Write a function named `oddOrEven` that accepts a`number` and returns
+
+  - `Number is odd` for odd number
+  - `Number is even` for even number.
+
+Do the following with above problem.
+*/
+
+// - Write a Function Declaration
+
+// function oddOrEven (number){
+//   return number%2 ==0? `Number is even` : `Number is odd`;
+// }
+
+// - Write an anonymous Function Expression
+
+// const oddOrEven = function(number){
+//   return number%2 ==0? `Number is even` : `Number is odd`;
+// }
+
+// - Write an named Function Expression
+
+// const oddOrEven = function oddOrEven(number){
+//   return number%2 ==0? `Number is even` : `Number is odd`;
+// }
+
+// - Write an Arrow Function without curly brackets (hint: use ternary operator)
+
+// const oddOrEven = (number) => number%2 ==0? `Number is even` : `Number is odd`;
+
+// - Write an Arrow Function with curly brackets
+
+// const oddOrEven = (number) => {return number%2 ==0? `Number is even` : `Number is odd`};
+
+// - Execute the function
+
+// oddOrEven();
+
+// - Execute the function and store the return value in a variable
+
+// let oddOrEvenFunction = oddOrEVen();
+
+// - What is the typeof returnValue
+
+// typeof(oddOrEvenFunction); //number
+
+
